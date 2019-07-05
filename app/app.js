eb6ea8f5e4b322fc5c07c29d09b6776794b38977
Jefferson Brito <jefersonbritoalm@gmail.com>
2019-07-05 18:43:47 -0300

    Subir para o site

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 // MENU NAVBAR -----------------------------------
-var offset = $('#meuMenu').offset().top;
-var $meuMenu = $('#meuMenu');
-$(document).on('scroll', function () {
-    if (offset <= $(window).scrollTop()) {
-        $meuMenu.addClass('fixar');
-    } else {
-        $meuMenu.removeClass('fixar');
-    }
-});
+// var offset = $('#meuMenu').offset().top;
+// var $meuMenu = $('#meuMenu');
+// $(document).on('scroll', function () {
+//     if (offset <= $(window).scrollTop()) {
+//         $meuMenu.addClass('fixar');
+//     } else {
+//         $meuMenu.removeClass('fixar');
+//     }
+// });
 
 // SLIDE ---------------------------------
 window.addEventListener('scroll', function(e){
@@ -16,4 +16,20 @@ window.addEventListener('scroll', function(e){
   // You can adjust the 0.4 to change the speed
 	var coords = (scrolled * 0.4) + 'px'
   parallax.style.transform = 'translateY(' + coords + ')';
+});
+// COR NAVBAR -------------------------------------
+$(document).ready(function(){       
+  var scroll_start = 10;
+  var startchange = $('#startchange');
+  var offset = startchange.offset();
+   if (startchange.length){
+  $(document).scroll(function() { 
+     scroll_start = $(this).scrollTop();
+     if(scroll_start > offset.top) {
+         $(".navbar").css('background-color', 'white');
+      } else {
+         $('.navbar').css('background-color', 'transparent');
+      }
+  });
+   }
 });
