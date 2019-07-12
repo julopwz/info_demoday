591e183ac7c6318259e26996b92a9c3fc33fe54c
julia <julialopes@usp.br>
2019-07-12 13:52:15 -0300

    subir no github ultimas modificações na models, index e sobre-nos

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Lugar(models.Model):
     nome = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
-    texto = models.TextField()
+    texto = models.TextField(default='')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -28,11 +28,11 @@ class Loja(models.Model):
         ('Cm','Cosmeticos'),
     ]
 
-    preco = models.DecimalField(decimal_places=2, max_digits=10, default=30)
-    lugar = models.CharField(max_length=30)
-    nome = models.ForeignKey(Lugar, on_delete=models.SET_DEFAULT, default='')
+    nome = models.CharField(max_length=30)
+    descricao = models.TextField(default='')
+    lugar = models.CharField(max_length=50)
     produtos = models.CharField(max_length=2, choices=produtos_opcoes)
-    foto = models.ImageField(upload_to='', default = '')
+    foto = models.ImageField(upload_to='', default= '')
 
     def __str__(self):
         return self.nome
@@ -53,15 +53,22 @@ class Restaurante(models.Model):
     ]
 
     nome = models.CharField(max_length=30)
-    descricao = models.TextField()
+    descricao = models.TextField(default='')
     qualidade = models.PositiveIntegerField()
     categoria = models.CharField(max_length=20, choices=tipo_opcoes)
     nivel_avaliacao = models.PositiveIntegerField(choices=nivel_avaliacao)
-    foto = models.ImageField(upload_to='', default = '')
+    foto = models.ImageField(upload_to='', default= '')
 
 class Servico(models.Model):
 
+    servico_opcoes = [
+        ('delivery', 'Delivery'),
+        ('outra', 'Outra'),
+        ('teste', 'Teste'),
+    ]
+
     nome = models.CharField(max_length=30)
-    descricao = models.TextField()
-    foto = models.ImageField(upload_to='', default = '')
+    descricao = models.TextField( max_length=50, default='')
+    foto = models.ImageField(upload_to='', default= '')
+    servico = models.CharField(max_length=2, choices=servico_opcoes)
 
