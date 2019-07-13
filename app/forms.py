57f8887a6efac32dc60b6f8ca9c29652dd94dbb4
Jefferson Brito <jefersonbritoalm@gmail.com>
2019-07-13 00:42:39 -0300

    Password ball

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from app.models import Cadastro
 
 
 class CadastroForm(forms.ModelForm):
+    senha = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = Cadastro
         fields = [
