5b2c84d1e38eac0409836aeaf08abbc44e66c804
Jefferson Brito <jefersonbritoalm@gmail.com>
2019-07-15 17:49:29 -0300

    heroku

--- infoveg/settings.py
+++ infoveg/settings.py
@@ -122,8 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_URL = '/static/'
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 django_heroku.settings(locals())
