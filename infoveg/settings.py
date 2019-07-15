34fc0d7253501ed84c12e958ebb74cde8298c168
Jefferson Brito <jefersonbritoalm@gmail.com>
2019-07-15 17:40:55 -0300

    heroku

--- infoveg/settings.py
+++ infoveg/settings.py
@@ -122,13 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
 django_heroku.settings(locals())
