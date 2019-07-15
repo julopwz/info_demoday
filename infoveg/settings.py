d8a1af856618d74d41e21cdd7b3274fd599b46ac
Jefferson Brito <jefersonbritoalm@gmail.com>
2019-07-15 17:43:31 -0300

    heroku

--- infoveg/settings.py
+++ infoveg/settings.py
@@ -122,10 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
