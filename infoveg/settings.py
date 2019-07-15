8349a6daddee0b151447eb638d7077c76ae4d62f
Jefferson Brito <jefersonbritoalm@gmail.com>
2019-07-15 04:14:15 -0300

    initial commit

--- infoveg/settings.py
+++ infoveg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,4 +127,4 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
